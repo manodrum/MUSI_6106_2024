b47c6026325c44d61ea9a7d4b257b50bfb9c9a19
TeAiris <64119271+TeAiris@users.noreply.github.com>
2024-01-29 19:21:01 -0500

    Working Output
    The output file is now writing to two columns as it should.

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,31 @@ use std::{fs::File, io::Write};
 use hound::WavReader;
 
 fn show_info() {
-    eprintln!("MUSI-6106 Assignment Executable");
-    eprintln!("(c) 2024 Stephen Garrett & Ian Clester");
+  eprintln!("MUSI-6106 Assignment Executable"); 
+  eprintln!("(c) 2024 Stephen Garrett & Ian Clester");
 }
 
 fn main() {
-    show_info();
+  show_info();
 
-    // Parse command line arguments
-    // First argument is input .wav file, second argument is output text file.
-    let args: Vec<String> = std::env::args().collect();
+  // Parse command line arguments
+  // First argument is input .wav file, second argument is output text file.
+  let args: Vec<String> = std::env::args().collect();
 
-    // Open the input wave file and determine number of channels
-    let mut reader = WavReader::open(&args[1]).unwrap();
-    let num_channels = reader.spec().channels;
+  // Open the input wave file and determine number of channels
+  let mut audio_in  = WavReader::open(&args[1]).unwrap();
+  let num_channels = audio_in.spec().channels as usize;
 
-    // Open the output text file
-    let mut out_file = File::create(&args[2]).unwrap();
+  // Open the output text file
+  let mut out_text = File::create(&args[2]).unwrap();
 
-    // Read audio data and write it to the output text file (one column per channel)
-    for sample in reader.samples::<i16>() {
-        let sample = sample.unwrap() as f32 / i16::MAX as f32;
-        write!(out_file, "{:.6}\n", sample).unwrap();
+  // Read audio data and write it to the output file (one column per channel)
+  for (i, sample) in audio_in.samples::<i16>().enumerate() {
+    let sample = sample.unwrap() as f32 / i16::MAX as f32;
+    if i % num_channels == 0 {
+      write!(out_text, "{:.6} ", sample).unwrap();
+    } else {
+      write!(out_text, "{:.6}\n", sample).unwrap(); 
     }
+  }
 }
