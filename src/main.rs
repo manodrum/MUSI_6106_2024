f0f0334817f52852785f4a0ee31d91507533cce8
yellowwave.sr <43238578+YellowWaveSr@users.noreply.github.com>
2024-02-06 14:36:11 -0500

    exercise-2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{fs::File, io::Write};
-
+use crate::ring_buffer::RingBuffer;
 mod ring_buffer;
 
 fn show_info() {
@@ -7,25 +7,79 @@ fn show_info() {
     eprintln!("(c) 2024 Stephen Garrett & Ian Clester");
 }
 
+
+
+
+
 fn main() {
    show_info();
+    // exercise 1
+    // // Parse command line arguments
+    // let args: Vec<String> = std::env::args().collect();
+    // if args.len() < 3 {
+    //     eprintln!("Usage: {} <input wave filename> <output text filename>", args[0]);
+    //     return
+    // }
 
-    // Parse command line arguments
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: {} <input wave filename> <output text filename>", args[0]);
-        return
+    // // Open the input wave file
+    // let mut reader = hound::WavReader::open(&args[1]).unwrap();
+    // let spec = reader.spec();
+    // let channels = spec.channels;
+
+    // // Read audio data and write it to the output text file (one column per channel)
+    // let mut out = File::create(&args[2]).expect("Unable to create file");
+    // for (i, sample) in reader.samples::<i16>().enumerate() {
+    //     let sample = sample.unwrap() as f32 / (1 << 15) as f32;
+    //     write!(out, "{}{}", sample, if i % channels as usize == (channels - 1).into() { "\n" } else { " " }).unwrap();
+    // }
+    
+    // exercise 2
+    let mut buffer = RingBuffer::new(10);
+    
+    // push two elements
+    buffer.push(42);
+    buffer.push(99);
+    buffer.display_buffer(|item| format!("{}", item));
+
+    // pop an element
+    buffer.pop();
+    buffer.display_buffer(|item| format!("{}", item));
+
+
+    // Append an element to the buffer
+    buffer.put(55);
+    buffer.display_buffer(|item| format!("{}", item));
+
+    // Peek at the element at the current read index
+    if let Some(value) = buffer.peek() {
+        println!("Peeked value at the current read index: {:?}", value);
+    } else {
+        println!("Buffer is empty, cannot peek.");
     }
+    buffer.display_buffer(|item| format!("{}", item));
 
-    // Open the input wave file
-    let mut reader = hound::WavReader::open(&args[1]).unwrap();
-    let spec = reader.spec();
-    let channels = spec.channels;
-
-    // Read audio data and write it to the output text file (one column per channel)
-    let mut out = File::create(&args[2]).expect("Unable to create file");
-    for (i, sample) in reader.samples::<i16>().enumerate() {
-        let sample = sample.unwrap() as f32 / (1 << 15) as f32;
-        write!(out, "{}{}", sample, if i % channels as usize == (channels - 1).into() { "\n" } else { " " }).unwrap();
+    // Get the element at a specific offset without removing it
+    if let Some(value) = buffer.get(1) {
+        println!("Got value at offset 1: {:?}", value);
+    } else {
+        println!("Index out of bounds, cannot get.");
     }
+    buffer.display_buffer(|item| format!("{}", item));
+
+    // Reset the buffer
+    buffer.reset();
+    buffer.display_buffer(|item| format!("{}", item));
+
+    println!("Length: {}", buffer.len());
+    println!("Capacity: {}", buffer.capacity());
+    println!("Get read index: {}", buffer.get_read_index());
+    buffer.set_read_index(5);
+    println!("Get read index after set: {}", buffer.get_read_index());
+    println!("Get write index: {}", buffer.get_write_index());
+    buffer.set_write_index(4);
+    println!("Get write index after set: {}", buffer.get_write_index());
+    
+
+
+    
 }
--- src/ring_buffer.rs
+++ src/ring_buffer.rs
@@ -1,64 +1,113 @@
 pub struct RingBuffer<T> {
     // TODO: fill this in.
+    buffer: Vec<T>,
+    read_index: usize,
+    write_index: usize,
+
 }
 
 impl<T: Copy + Default> RingBuffer<T> {
     pub fn new(length: usize) -> Self {
         // Create a new RingBuffer with `length` slots and "default" values.
         // Hint: look into `vec!` and the `Default` trait.
-        todo!()
+        let buffer = Vec::with_capacity(length);
+        let read_index = 0;
+        let write_index = 0;
+        RingBuffer { buffer, read_index, write_index }
     }
 
     pub fn reset(&mut self) {
         // Clear internal buffer and reset indices.
-        todo!()
+        self.buffer.clear();
+        self.read_index = 0;
+        self.write_index = 0;
     }
 
     // `put` and `peek` write/read without advancing the indices.
     pub fn put(&mut self, value: T) {
-        todo!()
+        if self.write_index < self.buffer.capacity() {
+            self.buffer.push(value);
+            self.write_index += 1;
+        } else {
+            println!("Buffer is full, cannot put more elements.");
+        }
     }
 
-    pub fn peek(&self) -> T {
-        todo!()
+    pub fn peek(&self) -> Option<&T> {
+        if self.read_index < self.write_index {
+            Some(&self.buffer[self.read_index])
+        } else {
+            None
+        }
     }
 
-    pub fn get(&self, offset: usize) -> T {
-        todo!()
+    pub fn get(&self, offset: usize) -> Option<&T> {
+        let index = self.read_index + offset;
+        if index < self.write_index {
+            Some(&self.buffer[index])
+        } else {
+            None
+        }
     }
 
     // `push` and `pop` write/read and advance the indices.
     pub fn push(&mut self, value: T) {
-        todo!()
+        if self.write_index < self.buffer.capacity() {
+            self.buffer.push(value);
+            self.write_index += 1;
+        } else {
+            println!("Buffer is full, cannot push more elements.");
+        }
     }
 
-    pub fn pop(&mut self) -> T {
-        todo!()
+    pub fn pop(&mut self) -> Option<T> {
+        if self.read_index < self.write_index {
+            let value = std::mem::replace(&mut self.buffer[self.read_index], Default::default());
+            self.read_index += 1;
+            Some(value)
+        }else {
+            None
+        }
     }
 
     pub fn get_read_index(&self) -> usize {
-        todo!()
+        self.read_index
     }
 
     pub fn set_read_index(&mut self, index: usize) {
-        todo!()
+        self.read_index = index;
     }
 
     pub fn get_write_index(&self) -> usize {
-        todo!()
+        self.write_index
     }
 
     pub fn set_write_index(&mut self, index: usize) {
-        todo!()
+        self.write_index = index;
     }
 
     pub fn len(&self) -> usize {
         // Return number of values currently in the buffer.
-        todo!()
+        self.write_index - self.read_index
     }
 
     pub fn capacity(&self) -> usize {
         // Return the length of the internal buffer.
-        todo!()
+        self.buffer.capacity()
+    }
+
+    pub fn display_buffer<F>(&mut self, formatter: F)
+    where
+        F: Fn(&T) -> String,
+        {
+        println!("Current buffer:");
+        let formatted_data: Vec<String> = self.buffer.iter().map(|item| formatter(item)).collect();
+        println!("Buffer Data: {:?}", formatted_data);
+        println!("Read Index: {}", self.read_index);
+        println!("Write Index: {}", self.write_index);
+        println!("Buffer Length: {}", self.len());
+        println!("Buffer Capacity: {}", self.capacity());
+        println!();
+
     }
 }
