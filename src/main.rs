a6726b8d699ee74b186bce5b9fc3ea948bdb03ff
Joseph Cleveland <josephcleveland@lawn-128-61-48-213.lawn.gatech.edu>
2024-02-28 14:17:06 -0500

    first pass on main function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{fs::File, io::Write};
 
+use vibrato::Vibrato;
+
 mod ring_buffer;
 mod vibrato;
 mod lfo;
@@ -23,11 +25,54 @@ fn main() {
     let mut reader = hound::WavReader::open(&args[1]).unwrap();
     let spec = reader.spec();
     let channels = spec.channels;
+    let block_size = 1024;
+
+    let writer_spec = hound::WavSpec {
+        channels: channels,
+        sample_rate: spec.sample_rate,
+        bits_per_sample: 32,
+        sample_format: hound::SampleFormat::Float
+    };
+    let mut writer = hound::WavWriter::create(&args[2], writer_spec).unwrap();
+
+    let mut vibratos: Vec<Vibrato> = Vec::new();
+
+    for c in 0..channels {
+        vibratos.push(
+            Vibrato::new(1.0, 1.0, spec.sample_rate as usize)
+        );
+    }
+    let mut input_buffer: Vec<Vec<f32>> = Vec::new();
+    let mut output_buffer: Vec<Vec<f32>> = Vec::new();
+    for i in 0..channels {
+        input_buffer.push(vec![0f32; block_size]);
+        output_buffer.push(vec![0f32; block_size]);
+    }
 
     // Read audio data and write it to the output text file (one column per channel)
-    let mut out = File::create(&args[2]).expect("Unable to create file");
+    // let mut out = File::create(&args[2]).expect("Unable to create file");
+
     for (i, sample) in reader.samples::<i16>().enumerate() {
         let sample = sample.unwrap() as f32 / (1 << 15) as f32;
-        write!(out, "{}{}", sample, if i % channels as usize == (channels - 1).into() { "\n" } else { " " }).unwrap();
+        input_buffer[i % channels as usize].push(sample);
+
+        // Once the input buffer is full up, process the block
+        if (i + 1) / channels as usize % block_size == 0 {
+            for c in 0..channels as usize {
+                vibratos[c].process_block(input_buffer[c].as_slice(), output_buffer[c].as_mut_slice());
+                input_buffer[c].clear();
+            }
+
+            for i in 0..block_size {
+                for c in 0..channels as usize {
+                    writer.write_sample(output_buffer[c][i]);
+                }
+            }
+
+            for c in 0..channels as usize {
+                output_buffer[c].clear();
+                input_buffer[c].clear();
+            }
+        }
     }
 }
--- src/vibrato.rs
+++ src/vibrato.rs
@@ -3,14 +3,31 @@ use crate::lfo::LFO;
 
 pub struct Vibrato {
     buffer: RingBuffer<f32>,
-    lfo: LFO
+    lfo: LFO,
+    delay: f32
 }
 
 impl Vibrato {
     pub fn new(freq: f32, max_delay_secs: f32, sample_rate: usize) -> Self {
         Vibrato {
             buffer: RingBuffer::new((max_delay_secs * sample_rate as f32) as usize),
-            lfo: LFO::new(freq, sample_rate)
+            lfo: LFO::new(freq, sample_rate),
+            delay: 0.0
+        }
+    }
+
+    pub fn set_delay(&mut self, delay: f32) {
+        self.delay = delay;
+    }
+
+    pub fn process_block(&mut self, input: &[f32], output: &mut [f32]) {
+        let mut lfo_samples = vec![0.0; output.len()];
+        self.lfo.get_block(lfo_samples.as_mut_slice());
+
+        for i in 0..input.len() {
+            self.buffer.push(input[i]);
+            let offset = lfo_samples[i] * self.delay;
+            output[i] = self.buffer.get_frac(offset);
         }
     }
 }
